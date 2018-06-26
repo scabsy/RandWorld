a217c0a67a528407a2c680679c736a43c54c2c85
scabsy <samuel.brownley@hotmail.com>
2018-06-26 17:40:39 +0800

    World Generation + Block placement
    world is generated better
    blocks can be placed (default block only)

--- level1.lua
+++ level1.lua
@@ -50,11 +50,13 @@ local World={}
 local WorldDisplay = {}
 local BlockTypes={"d","g","s"}
 local blockSize = 60
-local levelWidth = 1000
 local currentSection = 5
 local loadedSecCount = 3
 local sectionSize = 25
 
+
+local maxWorldWidth = 1024
+local maxWorldHeight = 64
 --camera
 local camera = perspective.createView()
 
@@ -123,55 +125,194 @@ function StopMovement()
 	Runtime:removeEventListener("enterFrame", pMove)
 end
 
+function GetPlayerGridLoc()
+	local GridLoc={math.floor(player.x/blockSize)+1,-math.floor(player.y/blockSize)}
+	return GridLoc
+end
+
 -----------world
+
+--create block
+function CreateBlock(blkType)
+	local block=display.newRect(0,0,blockSize,blockSize)
+	
+	if blkType~="c" then
+		physics.addBody(block,"static",{bounce=0})
+	else
+		block.alpha=0.1
+	end
+	
+	block.blockType=blkType
+	if block.blockType=="g" then
+		block:setFillColor(0,.6,0)
+	elseif block.blockType=="d" then
+		block:setFillColor(.5,.3,.05)
+	elseif block.blockType=="s" then
+		block:setFillColor(.4,.4,.4)
+	elseif block.blockType=="b" then
+		block:setFillColor(.1,.1,.1)
+	end
+				
+	block.WorldLoc = {i,j}
+	block.durability=100
+	
+	block:addEventListener("mouse",mineOnTouch)
+	-- block:addEventListener("touch",placeOnTouch)
+	
+	return block
+end
+
+function SetBlock(block,blkType)
+	if blkType~="c" then		
+		block.alpha=1
+		physics.addBody(block,"static",{bounce=0})		
+	else
+		physics.removeBody(block)	
+		block:setFillColor(1)		
+		block.alpha=0.1
+	end
+	
+	block.blockType=blkType
+	if block.blockType=="g" then
+		block:setFillColor(0,.6,0)
+	elseif block.blockType=="d" then
+		block:setFillColor(.5,.3,.05)
+	elseif block.blockType=="s" then
+		block:setFillColor(.4,.4,.4)
+	elseif block.blockType=="b" then
+		block:setFillColor(.1,.1,.1)
+	end
+	
+	World[block.WorldLoc[1]][block.WorldLoc[2]]=blkType
+	block.durability=100
+	
+	-- print(player.x,player.y)
+	-- print(GetPlayerGridLoc()[1],GetPlayerGridLoc()[2])
+end
+
 --mining
 local mineTarget
 function mineBlock()
 	--local tBlock = e.target
 	if mineTarget ~= nil then
-		if mineTarget.durability>0 then
-			mineTarget.durability = mineTarget.durability - 1
-			if mineTarget.durability <= 0 then
-				mineTarget.isVisible=false
-				physics.removeBody(mineTarget)
-				mineTarget.blockType="c"
-				World[mineTarget.WorldLoc[1]][mineTarget.WorldLoc[2]]="c"
-				mineTarget:removeEventListener("touch",mineOnTouch)
-				Runtime:removeEventListener("enterFrame",mineBlock)
+		if mineTarget.blockType~="b" then
+			if mineTarget.durability>0 then
+				mineTarget.durability = mineTarget.durability - 100
+				mineTarget.alpha = mineTarget.durability/100 + .1
+				if mineTarget.durability <= 0 then
+					SetBlock(mineTarget,"c")
+					Runtime:removeEventListener("enterFrame",mineBlock)
+				end
 			end
 		end
 	end
 end
 
+local wasPriButDown = false
+local wasSecButDown = false
 function mineOnTouch(e)	
-	if e.phase=="began" then
-		mineTarget=e.target
-		Runtime:addEventListener("enterFrame",mineBlock)
-	elseif e.phase=="moved" then
-		if e.target~=mineTarget then
-			Runtime:removeEventListener("enterFrame",mineBlock)
-			mineTarget=nil
+	mineTarget=e.target
+	if e.isPrimaryButtonDown then
+		if not wasPriButDown then
+			wasPriButDown = true
+			if mineTarget.blockType~="c" then
+				Runtime:addEventListener("enterFrame",mineBlock)
+			end
 		end
-	elseif e.phase=="ended" then
+	elseif not e.isPrimaryButtonDown then
+		wasPriButDown = false
+		
 		Runtime:removeEventListener("enterFrame",mineBlock)
 		if mineTarget~=nil then
 			if mineTarget.blockType~="c" then
 				mineTarget.durability=100
+				mineTarget.alpha=1
 			end
 		end
-		mineTarget=nil
 	end
+	
+	if e.isSecondaryButtonDown then
+		if not wasSecButDown then
+			wasSecButDown = true
+			if mineTarget.blockType=="c" then
+				if GetPlayerGridLoc()[1]~=mineTarget.WorldLoc[1] or (GetPlayerGridLoc()[2]~=mineTarget.WorldLoc[2] and GetPlayerGridLoc()[2]+1~=mineTarget.WorldLoc[2]) then
+					SetBlock(mineTarget,"d")
+				end
+			end
+		end
+	elseif not e.isSecondaryButtonDown then
+		wasSecButDown = false
+	end
+	
+	
+	-- if e.phase=="began" then
+		-- print("asc")
+		-- mineTarget=e.target
+		-- if e.isPrimaryButtonDown then
+			-- Runtime:addEventListener("enterFrame",mineBlock)
+		-- elseif e.isSecondaryButtonDown then
+			-- if GetPlayerGridLoc()[1]~=mineTarget.WorldLoc[1] or (GetPlayerGridLoc()[2]~=mineTarget.WorldLoc[2] and GetPlayerGridLoc()[2]+1~=mineTarget.WorldLoc[2]) then
+				-- SetBlock(mineTarget,"d")
+			-- end
+		-- end
+		
+	-- elseif e.phase=="moved" then
+		-- if e.target~=mineTarget then
+			-- Runtime:removeEventListener("enterFrame",mineBlock)
+			-- mineTarget=nil
+		-- end
+	-- elseif e.phase=="ended" then
+		-- Runtime:removeEventListener("enterFrame",mineBlock)
+		-- if mineTarget~=nil then
+			-- if mineTarget.blockType~="c" then
+				-- mineTarget.durability=100
+			-- end
+		-- end
+		-- mineTarget=nil
+	-- end
 end
 
 function GenerateWorld()
-	local height = 25
-	for i=1,levelWidth do
+	local height = maxWorldHeight/2+math.random(-5,5)
+	for i=1,maxWorldWidth do
 		World[i]={}
 		height=height + math.random(-2,2)
-		if height<1 then height = 1 end
-		for j=1,height do
-			local blkType = BlockTypes[math.random(#BlockTypes)]
-			World[i][j]=blkType
+		if height<maxWorldHeight/3 then height = maxWorldHeight/3 end
+		if height>maxWorldHeight/3*2 then height=maxWorldHeight/3*2 end
+		World[i].height=height
+		print(i,height,World[i].height)
+		for j=1,maxWorldHeight do
+			if j <= height then
+				-- local blkType = BlockTypes[math.random(#BlockTypes)]
+				World[i][j]="p"
+			else
+				World[i][j]="c"
+			end
+		end		
+	end
+	
+	print(maxWorldWidth)
+	for i=1,maxWorldWidth do
+		local rowHeight = math.floor(World[i].height)
+		for j=1,rowHeight do
+			-- local dirtHeight = math.floor((World[i].height-1)-math.random(World[i].height/3))
+			local dirtHeight = rowHeight-(rowHeight/3)-math.random(-5,5)
+			local stoneHeight = dirtHeight-(rowHeight/3)-math.random(-5,5)
+			if j>=dirtHeight then
+				World[i][j]="d"
+			end
+			
+			if j<dirtHeight then -- and j>stoneHeight then
+				World[i][j]="s"
+			end
+		
+			if j==rowHeight then
+				World[i][j]="g"
+			end
+			
+			if j<=1+math.random(0,2) then
+				World[i][j]="b"
+			end
 		end
 	end
 	--print(#World)
@@ -196,7 +337,7 @@ function CreateWorld(worldArray)
 	local minSec = ((currentSection-2)*sectionSize)+1
 	local maxSec = ((currentSection)*sectionSize)+sectionSize
 	if minSec<1 then minSec=1 end
-	if maxSec>levelWidth then maxSec=levelWidth end
+	if maxSec>maxWorldWidth then maxSec=maxWorldWidth end
 	
 	for i=minSec,maxSec do
 		WorldDisplay[wdNum]={}
@@ -204,33 +345,14 @@ function CreateWorld(worldArray)
 			local tmpBlkType = World[i][j]
 			--World[i][j]=nil
 			if type(tmpBlkType)~= "table"  then 
-				WorldDisplay[wdNum][j]=display.newRect(0,0,blockSize,blockSize)
-				WorldDisplay[wdNum][j].x,WorldDisplay[wdNum][j].y = (i*blockSize)-blockSize/2,(screenH-j*blockSize)+blockSize/2
-				
-				if tmpBlkType~="c" then
-					physics.addBody(WorldDisplay[wdNum][j],"static",{bounce=0})
-				else
-					WorldDisplay[wdNum][j].alpha=0
-				end
-				
-				WorldDisplay[wdNum][j].blockType=tmpBlkType
-				if WorldDisplay[wdNum][j].blockType=="g" then
-					WorldDisplay[wdNum][j]:setFillColor(0,1,0)
-				elseif WorldDisplay[wdNum][j].blockType=="d" then
-					WorldDisplay[wdNum][j]:setFillColor(1,0,1)
-				elseif WorldDisplay[wdNum][j].blockType=="s" then
-					WorldDisplay[wdNum][j]:setFillColor(.4,.4,.4)
-				end
-							
+				WorldDisplay[wdNum][j]=CreateBlock(tmpBlkType)
+				WorldDisplay[wdNum][j].x,WorldDisplay[wdNum][j].y = (i*blockSize)-blockSize/2,(-j*blockSize)+blockSize/2					
 				WorldDisplay[wdNum][j].WorldLoc = {i,j}
-				WorldDisplay[wdNum][j].durability=100
 				
-				WorldDisplay[wdNum][j]:addEventListener("touch",mineOnTouch)
 				camera:add(WorldDisplay[wdNum][j],2)
 			end
 		end
 		wdNum = wdNum+1
-		--print("end line")
 	end	
 	
 end
@@ -263,7 +385,7 @@ function LoadWorld()
 	else
 		World={}
 		local lineN = 0
-		for i=1,levelWidth do			
+		for i=1,maxWorldWidth do			
 			local line = file:read("*l")
 			-- lineN=lineN+1
 			-- if lineN >= currentSection*sectionSize then
@@ -383,8 +505,8 @@ function scene:create( event )
 		-- end
 	-- end
 	
-	-- camera:setBounds(0,levelWidth*blockSize-screenW/2,-3300,screenH/2)
-	camera:setBounds(screenW/2,levelWidth*blockSize-screenW/2,-3300,screenH/2)
+	-- camera:setBounds(0,maxWorldWidth*blockSize-screenW/2,-3300,screenH/2)
+	camera:setBounds(screenW/2,maxWorldWidth*blockSize-screenW/2,-maxWorldHeight*blockSize,-screenH/2)
 	
 	camera.damping=10
 	camera:setFocus(player)
